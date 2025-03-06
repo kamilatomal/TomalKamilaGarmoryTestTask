df175f5417db57247bd36197e46e8f9f0cfc2877
kamilatomal <tomalkamila@gmail.com>
2025-03-06 13:56:56 +0100

    ItemData and ItemsData scripts created.

---
+++ TomalKamilaGarmoryTestTask/Assets/Scripts/ItemData.cs
@@ -0,0 +1,15 @@
+[System.Serializable]
+public class ItemData
+{
+    public string Name;
+    public string Category;
+    public int Rarity;
+    public int Damage;
+    public int HealthPoints;
+    public int Defense;
+    public float LifeSteal;
+    public float CriticalStrikeChance;
+    public float AttackSpeed;
+    public float MovementSpeed;
+    public float Luck;
+}
