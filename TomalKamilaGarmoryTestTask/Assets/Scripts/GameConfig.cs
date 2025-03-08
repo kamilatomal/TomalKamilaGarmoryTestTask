7887b9d3d21b856226a362dc19d574096e297021
kamilatomal <tomalkamila@gmail.com>
2025-03-08 20:35:16 +0100

    GameConfig script and SO created

---
+++ TomalKamilaGarmoryTestTask/Assets/Scripts/GameConfig.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameConfig", menuName = "Game/GameConfig")]
+public class GameConfig : ScriptableObject
+{
+    #region non public fields
+
+    [SerializeField]
+    private ItemsConfig _itemsConfig;
+  
+    #endregion
+
+    #region public fields
+
+    
+    #endregion
+
+    #region non public methods
+
+    #endregion
+
+    #region public methods
+    
+    public ItemsConfig GetItemsConfig()
+    {
+        return _itemsConfig;
+    }
+    
+    #endregion
+}
