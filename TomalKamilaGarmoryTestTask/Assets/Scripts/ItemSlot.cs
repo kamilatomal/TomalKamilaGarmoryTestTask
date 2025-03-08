b9733b5478097026437080d5d0e6a7bd332a3c5a
kamilatomal <tomalkamila@gmail.com>
2025-03-08 13:08:30 +0100

    ItemSlot script created

---
+++ TomalKamilaGarmoryTestTask/Assets/Scripts/ItemSlot.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlot : MonoBehaviour
+{
+    #region non public fields
+    
+    [SerializeField]
+    private Image _image;
+    
+    #endregion
+
+    #region public fields
+
+    #endregion
+
+    #region non public methods
+
+    #endregion
+
+    #region public methods
+
+    #endregion
+}
