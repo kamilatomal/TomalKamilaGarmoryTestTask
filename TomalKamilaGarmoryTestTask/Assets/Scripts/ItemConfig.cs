96e7e06214a76e9b673aeca2fdfa5c728bad5763
kamilatomal <tomalkamila@gmail.com>
2025-03-08 17:07:08 +0100

    ItemsConfig SO created with ItemConfig list and dict. Populate dict with string key and ItemConfig value

--- TomalKamilaGarmoryTestTask/Assets/Scripts/ItemConfig.cs
+++ TomalKamilaGarmoryTestTask/Assets/Scripts/ItemConfig.cs
@@ -1,9 +1,28 @@
-using TMPro;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ItemConfig", menuName = "Item/ItemConfig")]
 public class ItemConfig : ScriptableObject
 {
+    #region non public fields
+
     [SerializeField]
     private Sprite _sprite;
+    [SerializeField]
+    public string ItemName;
+    
+    #endregion
+
+    #region public fields
+
+    #endregion
+
+    #region non public methods
+
+    #endregion
+
+    #region public methods
+    
+
+    #endregion
 }
+
