d5130f8b5d9021d7d46ea123861d4c1f75fe4640
kamilatomal <tomalkamila@gmail.com>
2025-03-08 12:34:13 +0100

    PlayerMovement script created

---
+++ TomalKamilaGarmoryTestTask/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region non public fields
+
+    #endregion
+
+    #region public fields
+
+    #endregion
+
+    #region non public methods
+
+    #endregion
+
+    #region public methods
+
+    #endregion
+}
