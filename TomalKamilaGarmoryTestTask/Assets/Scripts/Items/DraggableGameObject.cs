58a153d6a45d3ffa87fd3dab3f611fba5cabd9ca
kamilatomal <tomalkamila@gmail.com>
2025-03-09 18:49:07 +0100

    DraggableItem component attached to different game object

--- TomalKamilaGarmoryTestTask/Assets/Scripts/Items/DraggableGameObject.cs
+++ TomalKamilaGarmoryTestTask/Assets/Scripts/Items/DraggableGameObject.cs
@@ -10,8 +10,6 @@ namespace Items
         #region non public fields
 
         [SerializeField] 
-        private Image _image;
-        [SerializeField] 
         private Transform _parentWhileDragging;
 
         private Transform _originParent;
@@ -38,7 +36,6 @@ namespace Items
         {
             _originParent = transform.parent;
             transform.SetParent(_parentWhileDragging);
-            _image.raycastTarget = false;
         }
 
         public void OnDrag(PointerEventData eventData)
@@ -49,7 +46,6 @@ namespace Items
         public void OnEndDrag(PointerEventData eventData)
         {
             transform.SetParent(_originParent);
-            _image.raycastTarget = true;
             _onDragEnd?.Invoke(eventData);
         }
 
